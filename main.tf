a121a5e689b141f814a38fd98372cc1e7e3b39b9
Lee Fowler <lee.fowler@hashicorp.com>
2025-09-19 19:16:55 -0400

    refactor: rename hcp waypoint tfc config resource

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "hcp_waypoint_tfc_config" "test" {
+resource "hcp_waypoint_tfc_config" "hashicorp_sa_workshops" {
   token        = var.hcp_waypoint_tf_team_token
   tfc_org_name = var.hcp_waypoint_tf_organization
 }
